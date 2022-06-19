de5fa93e32f604f423b6dee2b96a7d24ba848fa1
Rahim2725 <info.abdurrahim12@gmail.com>
2022-06-20 01:21:30 +0600

    service on load dara

--- index.js
+++ index.js
@@ -9,25 +9,32 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.di2q1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
-        const phoneCollections = client.db('phoneShop').collection('service') ;
-        
-        app.get('/service', async(req, res) => {
-            const services = await phoneCollections.find().toArray() ;
-            console.log(services) ;
+        const phoneCollections = client.db('phoneShop').collection('service');
+
+        app.get('/service', async (req, res) => {
+            const services = await phoneCollections.find().toArray();
             res.send(services);
         });
-        
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id ;
+            const query = { _id:ObjectId(id) }
+            const service = await phoneCollections.findOne(query) ;
+            res.send(service);
+        })
+
+
 
 
     }
-    finally{
+    finally {
         // await client.close();
     }
 
